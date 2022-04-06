1a1373773dac0f278e9c39a4a9be7f84d69f35bc
Lucy <Luciekinyanjui>
2022-04-07 00:59:17 +0300

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,13 @@ import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
-}
 
+  return(
+  <div id="about">
+    <h2>About Me</h2>
+    <p>Checkout what I made</p>
+    <img src={image} alt="My Image"></img>
+  </div>
+);
+  }
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,13 @@ import React from "react";
 function NavBar() {
   // update the JSX being returned!
   return <nav>NavBar</nav>;
+  return(
+    <nav>
+      {/* NavBar */}
+      <a href="#home">Home</a>
+      <a href="#about">about</a>
+    </nav>
+  )
 }
 
 export default NavBar;
